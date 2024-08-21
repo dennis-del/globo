b08ad277ab11c4b268942139c3e3c27f096fe278
dennis-del <dennisjames991@gmail.com>
2024-08-21 17:00:38 +0530

    globo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AllProductComponent } from './all-product/all-product.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'',component:AllProductComponent},
+  {path:'cart',component:CartComponent},
+  {path:'checkout',component:CheckoutComponent},
+  {path:'view-product/:id',component:ViewProductComponent},
+  {path:'wishlist',component:WishlistComponent},
+  {path:'**',component:PagenotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
